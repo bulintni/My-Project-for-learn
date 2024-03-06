da2362f061bfa614becbc000e624e1a139b1978f
bulintni <154816395+bulintni@users.noreply.github.com>
2024-03-06 16:22:06 +0700

    React-first-projectforlearn

--- Landing page 02/Foodman_17-02-24-main/main.js
+++ Landing page 02/Foodman_17-02-24-main/main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const menuBtn = document.getElementById('menu-btn')
 const navLinks = document.getElementById('nav-links')
 const menuBtnIcon = document.querySelector('i')
@@ -85,4 +86,183 @@ const swiper = new Swiper(".swiper", {
   pagination: {
     el: ".swiper-pagination",
   },
+=======
+<<<<<<< HEAD
+const menuBtn = document.getElementById('menu-btn')
+const navLinks = document.getElementById('nav-links')
+const menuBtnIcon = document.querySelector('i')
+
+menuBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('open')
+
+  const isOpen = navLinks.classList.contains('open')
+  menuBtnIcon.setAttribute('class', isOpen?"ri-close-line":"ri-menu-line")
+})
+
+navLinks.addEventListener('click', (e) => {
+  navLinks.classList.remove("open")
+  menuBtnIcon.setAttribute('class', 'ri-menu-line')
+})
+
+const scrollRevealOption = {
+  distance:"50px",
+  origin:"bottom",
+  duration: 1000,
+}
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+})
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 500,
+})
+ScrollReveal().reveal(".header__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1000,
+})
+ScrollReveal().reveal(".header__content .header__btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+})
+
+ScrollReveal().reveal(".explore__image img", {
+  ...scrollRevealOption,
+  origin: "left",
+})
+ScrollReveal().reveal(".explore__content .section__header", {
+  ...scrollRevealOption,
+  delay: 500,
+})
+ScrollReveal().reveal(".explore__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1000,
+})
+ScrollReveal().reveal(".explore__content .explore__btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+})
+
+ScrollReveal().reveal(".banner__card", {
+  ...scrollRevealOption,
+  interval: 500,
+})
+
+// Chef-title
+
+ScrollReveal().reveal(".chef__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+})
+ScrollReveal().reveal(".chef__content .section__header", {
+  ...scrollRevealOption,
+  delay: 500,
+})
+ScrollReveal().reveal(".chef__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1000,
+})
+ScrollReveal().reveal(".chef__list li", {
+  ...scrollRevealOption,
+  delay: 1500,
+  interval: 500,
+})
+
+const swiper = new Swiper(".swiper", {
+  loop:true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+=======
+const menuBtn = document.getElementById('menu-btn')
+const navLinks = document.getElementById('nav-links')
+const menuBtnIcon = document.querySelector('i')
+
+menuBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('open')
+
+  const isOpen = navLinks.classList.contains('open')
+  menuBtnIcon.setAttribute('class', isOpen?"ri-close-line":"ri-menu-line")
+})
+
+navLinks.addEventListener('click', (e) => {
+  navLinks.classList.remove("open")
+  menuBtnIcon.setAttribute('class', 'ri-menu-line')
+})
+
+const scrollRevealOption = {
+  distance:"50px",
+  origin:"bottom",
+  duration: 1000,
+}
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+})
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 500,
+})
+ScrollReveal().reveal(".header__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1000,
+})
+ScrollReveal().reveal(".header__content .header__btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+})
+
+ScrollReveal().reveal(".explore__image img", {
+  ...scrollRevealOption,
+  origin: "left",
+})
+ScrollReveal().reveal(".explore__content .section__header", {
+  ...scrollRevealOption,
+  delay: 500,
+})
+ScrollReveal().reveal(".explore__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1000,
+})
+ScrollReveal().reveal(".explore__content .explore__btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+})
+
+ScrollReveal().reveal(".banner__card", {
+  ...scrollRevealOption,
+  interval: 500,
+})
+
+// Chef-title
+
+ScrollReveal().reveal(".chef__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+})
+ScrollReveal().reveal(".chef__content .section__header", {
+  ...scrollRevealOption,
+  delay: 500,
+})
+ScrollReveal().reveal(".chef__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1000,
+})
+ScrollReveal().reveal(".chef__list li", {
+  ...scrollRevealOption,
+  delay: 1500,
+  interval: 500,
+})
+
+const swiper = new Swiper(".swiper", {
+  loop:true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+>>>>>>> f4443d8a9f671cb5f5200d9281c6e56fe91ed77a
+>>>>>>> Stashed changes
 })
